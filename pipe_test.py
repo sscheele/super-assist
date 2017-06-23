733f987036eb405cc2c18bf05cc00062bcb26390
Sam <sam.scheele@outlook.com>
2017-06-23 18:44:48 -0400

    Add pipe_test.py

--- pipe_test.py
+++ pipe_test.py
@@ -1,13 +1,7 @@
-import io
-import time
 import subprocess
 import sys
-
-filename = 'test.log'
-with io.open(filename, 'wb') as writer, io.open(filename, 'rb', 1) as reader:
-    process = subprocess.Popen(["../GAssist/env/bin/google-assistant-demo"], stdout=writer)
-    while process.poll() is None:
-        sys.stdout.write(reader.read())
-        time.sleep(0.5)
-    # Read the remaining
-    sys.stdout.write(reader.read()) 
+with open('test.log', 'w') as f:
+    process = subprocess.Popen(["../GAssist/env/bin/google-assistant-demo"], stdout=subprocess.PIPE)
+    for line in iter(process.stdout.readline, ''):
+        sys.stdout.write(line)
+        f.write(line)
